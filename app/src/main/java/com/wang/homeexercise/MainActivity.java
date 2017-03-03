8f83a9165c89b40fc10671668735babdc1bfb95a
romantiskt <wyroman@163.com>
2017-03-03 15:28:41 +0800

    RecyclerView 的练习

--- app/src/main/java/com/wang/homeexercise/MainActivity.java
+++ app/src/main/java/com/wang/homeexercise/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wang.homeexercise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.wang.homeexercise.smoothlist.adapter.DividerAct;
 import com.wang.homeexercise.smoothlist.adapter.DividerAdapter;
 import com.wang.tools.base.BaseAct;
 
@@ -10,6 +14,7 @@ import java.util.List;
 public class MainActivity extends BaseAct {
     List<String> mData;
     DividerAdapter mDividerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +25,19 @@ public class MainActivity extends BaseAct {
 
     }
 
+    private Button btnDivider;
+
     @Override
     protected void initView() {
         setContentView(R.layout.activity_main);
 
+        btnDivider = (Button) findViewById(R.id.btn_divider);
+        btnDivider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 startActivity(new Intent(MainActivity.this, DividerAct.class));
+            }
+        });
     }
 
     @Override
